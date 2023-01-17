5b0c179c3a5ca14b89f0453a65099ca786bda554
João Borchoski <joaohenriqueborchoski@gmail.com>
2023-01-17 15:34:37 -0300

    create lis repos

--- src/pages/repos/indexRepos.jsx
+++ src/pages/repos/indexRepos.jsx
@@ -3,20 +3,60 @@ import { Header } from "../../components/header/header";
 import { StyledBodyHome } from "../home/styledHome";
 import { StyledReposMain } from "./styledRepos";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/userContext";
 
 export const Repos = () => {
+    const { userRepos } = useContext(UserContext);
+    console.log(userRepos[1]);
+
     return (
         <StyledBodyHome>
             <Header sobre={""} />
             <StyledReposMain>
                 <div>
                     <section>
-                        <h2>Aqui estão todos os meus projetos</h2>
+                        <h1>Aqui estão todos os meus projetos</h1>
                     </section>
                     <section>
                         <AiOutlineFundProjectionScreen size={70} />
                     </section>
                 </div>
+                <ul>
+                    {userRepos.map((element, i) => {
+                        return (
+                            <li>
+                                <h3>{element?.name}</h3>
+                                <p>{element?.description}</p>
+                                <section>
+                                    <a
+                                        href={element?.html_url}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        style={{
+                                            background:
+                                                "rgba(107, 211, 235, 0.8)",
+                                        }}
+                                    >
+                                        Repository
+                                    </a>
+                                    {element?.homepage !== "" ? (
+                                        <a
+                                            style={{
+                                                background: "#868e96",
+                                            }}
+                                            href={element?.homepage}
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            Demo
+                                        </a>
+                                    ) : null}
+                                </section>
+                            </li>
+                        );
+                    })}
+                </ul>
             </StyledReposMain>
         </StyledBodyHome>
     );
--- src/pages/repos/styledRepos.js
+++ src/pages/repos/styledRepos.js
@@ -8,6 +8,10 @@ export const StyledReposMain = styled.div`
 
     margin: 90px 0 30px 0;
 
+    @media (max-width: 1100px) {
+        padding: 0 5%;
+    }
+
     > div {
         display: flex;
 
@@ -15,10 +19,11 @@ export const StyledReposMain = styled.div`
 
         > section {
             border-radius: var(--radius2);
-            min-height: 10vh;
-            max-height: 10vh;
             background: #868e96;
             padding: 16px;
+
+            min-height: 7vh;
+            max-height: 7vh;
         }
 
         > section:nth-child(1) {
@@ -27,6 +32,11 @@ export const StyledReposMain = styled.div`
             flex-direction: column;
             justify-content: space-evenly;
             gap: 16px;
+
+            @media (max-width: 1100px) {
+                width: 100%;
+                max-height: 100%;
+            }
         }
 
         > section:nth-child(2) {
@@ -37,6 +47,83 @@ export const StyledReposMain = styled.div`
             justify-content: center;
             overflow: hidden;
             padding: 0;
+
+            @media (max-width: 1100px) {
+                display: none;
+            }
+        }
+    }
+
+    > ul {
+        border-radius: var(--radius2);
+        background: #868e96;
+        padding: 32px;
+
+        display: flex;
+        justify-content: space-evenly;
+        gap: 48px;
+        flex-wrap: wrap;
+
+        > li {
+            min-height: 30vh;
+            width: 30%;
+            max-width: 45%;
+            min-width: 300px;
+
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+
+            gap: 16px;
+            padding: 24px 16px;
+
+            border: 2px solid #212529;
+            border-radius: var(--radius2);
+
+            background: #9ea4ab;
+
+            transition: all 0.5s;
+
+            @media (max-width: 1100px) {
+                min-height: 30vh;
+                max-height: 100vh;
+            }
+
+            > p {
+                font-size: var(--font5);
+            }
+
+            > section {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+
+                width: 100%;
+
+                a {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+
+                    height: 40px;
+                    width: 45%;
+
+                    border-radius: var(--radius2);
+
+                    transition: all 0.5s;
+                }
+            }
+        }
+
+        li:hover {
+            background: #868e96;
+        }
+        li:hover a:nth-child(2) {
+            background: #787f87 !important;
+        }
+
+        a:hover {
+            scale: 1.1;
         }
     }
 `;
